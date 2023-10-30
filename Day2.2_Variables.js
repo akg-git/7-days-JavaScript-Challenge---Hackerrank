8f27d7d99eb7c6542526985a8306c622438f432a
Asish_AKG <105587312+akg-git@users.noreply.github.com>
2023-10-30 23:36:04 +0530

    Add files via upload
    Day2 Challenge Commit.

---
+++ Day2.2_Variables.js
@@ -0,0 +1,7 @@
+// 7 days of Javascript Challenge
+// Day: 2
+//Question-2: Variables
+//@Asish_AKG
+
+//Write your code below this line.
+var newVariable = "My First Variable";
---
+++ Day2.3_IntroducingArithmeticOperators.js
@@ -0,0 +1,17 @@
+// 7 days of Javascript Challenge
+// Day: 2
+//Question-3: Introducing Arithmetic Operators
+//@Asish_AKG
+
+//var a=5;
+//var b=7;
+
+//Do not declare variables `a` and `b`.
+//Write your code below this line.
+
+var add = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+var inc = ++a;
+var dec = --b;
